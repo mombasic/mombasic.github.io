aa8f0576e77631f387fc7fa03288d23c87a5da12
mombasic <mahir.ombasic@gmail.com>
2026-02-19 20:58:30 +0100

    okSadMozdaRadi

--- om.js
+++ om.js
@@ -3,36 +3,38 @@ let request = new XMLHttpRequest();
     request.send(null);
     let niz = JSON.parse(request.responseText);
 
+    let gdiv = document.getElementsByClassName("glavnidiv");
+
     console.log("molimTeBrzino");
 
     for (let i in niz.filmovi) {
-        document.getElementsByClassName("glavnidiv").innerHTML = 
+        gdiv[0].innerHTML = 
         '<div class="article">';
-        document.getElementsByClassName("glavnidiv").innerHTML = 
+        gdiv[0].innerHTML += 
                     '<div class="carddiv1">'
-        document.getElementsByClassName("glavnidiv").innerHTML = 
+        gdiv[0].innerHTML += 
                     '<img src=' + niz.filmovi[i].naziv +  '.webp" height="200px">'
-        document.getElementsByClassName("glavnidiv").innerHTML = 
+        gdiv[0].innerHTML += 
                     '</div>'
-        document.getElementsByClassName("glavnidiv").innerHTML = 
+        gdiv[0].innerHTML += 
                     '<div class="carddiv">'
-        document.getElementsByClassName("glavnidiv").innerHTML = 
+        gdiv[0].innerHTML += 
                     '<h5><img src="element_images/time.svg" width="16px" height="16px">' + niz.filmovi[i].datumRecenzije + '</h5>'
-        document.getElementsByClassName("glavnidiv").innerHTML = 
+        gdiv[0].innerHTML += 
                     '<h2>' + niz.filmovi[i].naziv + '(' + niz.filmovi[i].datumFilma + ')</h2>'
-        document.getElementsByClassName("glavnidiv").innerHTML = 
+        gdiv[0].innerHTML += 
                     '<p>' + niz.filmovi[i].recenzija + '</p>'
-        document.getElementsByClassName("glavnidiv").innerHTML = 
+        gdiv[0].innerHTML += 
                     '<div class="pomjeraj">'
-        document.getElementsByClassName("glavnidiv").innerHTML = 
+        gdiv[0].innerHTML += 
                     '<a href="review.html">Citajte Review</a>'
-        document.getElementsByClassName("glavnidiv").innerHTML = 
+        gdiv[0].innerHTML += 
                     '</div>'
-        document.getElementsByClassName("glavnidiv").innerHTML = 
+        gdiv[0].innerHTML += 
                     '</div>'
-        document.getElementsByClassName("glavnidiv").innerHTML = 
+        gdiv[0].innerHTML += 
                 '</div>'
-        document.getElementsByClassName("glavnidiv").innerHTML = 
+        gdiv[0].innerHTML += 
             '</div>'
         console.log("kurac" + i);
     }
