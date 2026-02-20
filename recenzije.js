54ef1ea90b4b78c26b734c9f0a116b3ae14a46e6
mombasic <mahir.ombasic@gmail.com>
2026-02-20 10:46:26 +0100

    cas 1.5

--- recenzije.js
+++ recenzije.js
@@ -25,7 +25,7 @@ let request = new XMLHttpRequest();
         tagovi += '<h2>' + niz.filmovi[i].naziv + '(' + niz.filmovi[i].godinaFilma.toString() + ')</h2>';
         tagovi +=  '<p>' + niz.filmovi[i].plot + '</p>';
         tagovi += '<div class="pomjeraj">';
-        tagovi += '<a href="review.html">Citajte Review</a>';
+        tagovi += '<a href="review.html" id="fudo">Citajte Review</a>';
         tagovi += '</div>';
         tagovi += '</div>';
         tagovi += '</div>';
